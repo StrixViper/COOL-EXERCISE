d9a9bed39704dfb03b08cb07c96246d37ddf6469
StrixViper <160177463+StrixViper@users.noreply.github.com>
2024-09-26 13:03:20 +0300

    Update var.c
    Now it will determine the type of

--- var.c
+++ var.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 // Enum to define the type of the variable
 typedef enum {
     VAR_TYPE_INT,
     VAR_TYPE_FLOAT,
-    VAR_TYPE_STRING
+    VAR_TYPE_STRING,
+    VAR_TYPE_UNKNOWN
 } VarType;
 
 // Union to hold different types of values
@@ -21,23 +24,45 @@ typedef struct {
     VarType type;
 } Var;
 
-// Function to set an integer value
-void setInt(Var *var, int value) {
-    var->value.intValue = value;
-    var->type = VAR_TYPE_INT;
+// Function to determine the variable type from a string
+VarType determineType(const char *value) {
+    char *endptr;
+
+    // Check if it is an integer
+    long intValue = strtol(value, &endptr, 10);
+    if (*endptr == '\0') {
+        return VAR_TYPE_INT;
+    }
+
+    // Check if it is a float
+    float floatValue = strtof(value, &endptr);
+    if (*endptr == '\0') {
+        return VAR_TYPE_FLOAT;
+    }
+
+    // Otherwise, it is a string
+    return VAR_TYPE_STRING;
 }
 
-// Function to set a float value
-void setFloat(Var *var, float value) {
-    var->value.floatValue = value;
-    var->type = VAR_TYPE_FLOAT;
-}
+// Function to set a value based on the input string
+void setValue(Var *var, const char *value) {
+    var->type = determineType(value);
 
-// Function to set a string value
-void setString(Var *var, const char *value) {
-    strncpy(var->value.strValue, value, sizeof(var->value.strValue) - 1);
-    var->value.strValue[sizeof(var->value.strValue) - 1] = '\0'; // Ensure null-termination
-    var->type = VAR_TYPE_STRING;
+    switch (var->type) {
+        case VAR_TYPE_INT:
+            var->value.intValue = strtol(value, NULL, 10);
+            break;
+        case VAR_TYPE_FLOAT:
+            var->value.floatValue = strtof(value, NULL);
+            break;
+        case VAR_TYPE_STRING:
+            strncpy(var->value.strValue, value, sizeof(var->value.strValue) - 1);
+            var->value.strValue[sizeof(var->value.strValue) - 1] = '\0'; // Ensure null-termination
+            break;
+        default:
+            // Handle unknown type case if necessary
+            break;
+    }
 }
 
 // Function to print the variable based on its type
@@ -62,13 +87,13 @@ void printVar(const Var *var) {
 int main() {
     Var myVar;
 
-    setInt(&myVar, 10);
+    setValue(&myVar, "10");
     printVar(&myVar);
 
-    setFloat(&myVar, 3.14f);
+    setValue(&myVar, "3.14");
     printVar(&myVar);
 
-    setString(&myVar, "Hello, World!");
+    setValue(&myVar, "Hello, World!");
     printVar(&myVar);
 
     return 0;
